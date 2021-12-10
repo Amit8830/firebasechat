6e30aee586e351fda6839704da9c0f1d92bd4707
Amit8830 <gamit9022@gmail.com>
2021-12-10 14:07:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1pQ2jYfRovaiS1Hp3qNxoahBpvb0kWDo",
+  authDomain: "react-form-fire.firebaseapp.com",
+  projectId: "react-form-fire",
+  storageBucket: "react-form-fire.appspot.com",
+  messagingSenderId: "605450819656",
+  appId: "1:605450819656:web:796f692ce47e43f4fda07d"
+};
+
+initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, user => {
+      setUser(user)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
